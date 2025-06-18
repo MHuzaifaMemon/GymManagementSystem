b7afed563437ebe028a1613539d0690f82d33b3d
Muhammad Huzaifa <Muhammadhuzaifa220011@gmail.com>
2025-06-18 15:26:05 +0500

    Completed frontend dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Sidebar from './Components/Sidebar/sidebar';
 import Dashboard from './Pages/Dashboard/dashboard';
 import Home from './Pages/Home/home';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,useNavigate } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+
 function App() {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(()=>{
+    let isLogedIn = sessionStorage.getItem("isLogin");
+    if(isLogedIn){
+      setIsLogin(true);
+      navigate('/dashboard')
+    } 
+    // }else{
+    //   setIsLogin(false)
+    //   navigate('/');
+    // }
+  },[localStorage.getItem("isLogin")])
+
+
   return (
-    <div className="">
+    <div className="flex">
+       {
+        isLogin && <Sidebar />
+      }
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
