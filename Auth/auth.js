e566c2bc74239e20143381ba1c827a0e51c90401
Muhammad Huzaifa <Muhammadhuzaifa220011@gmail.com>
2025-07-02 00:55:32 +0500

    ...

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,26 +1,4 @@
-// const Gym =require('../Modals/gym');
-// const jwt = require('jsonwebtoken');
-
-// const auth = async (req, res, next) => {
-//     try {
-//         const token = req.cookies.cookie_token; // Assuming the token is stored in a cookie named 'token'
-//         if (!token) {
-//             return res.status(401).json({ error: 'No token, autherization denied' });
-//         }
-
-//         // Verify the token
-//         const decode = jwt.verify(token, process.env.JWT_SecretKey); // Use your secret key from .env
-//         req.gym = await Gym.findById(decode.gym_id).select('-password'); // Find the gym by ID and exclude the password field
-//         next(); // Call next middleware if token is valid
-//     } catch (error) {
-//         res.status(400).json({ error: 'Invalid token' });
-//     }
-// };
-
-// module.exports = auth;
-// // This middleware checks for a valid JWT token in the request cookies.
-
-const Gym = require('../../Modals/gym');
+const Gym = require('../Modals/gym');
 const jwt = require('jsonwebtoken');
 
 const auth = async (req, res, next) => {
--- Routes/member.js
+++ Routes/member.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const MemberController = require('../Controllers/member');
-const auth = require('../backend/Auth/auth');
+const auth = require('../Auth/auth');
 
 router.get('/all-member',auth, MemberController.getAllMembers);
 router.post('/register-member', auth, MemberController.registerMember);
